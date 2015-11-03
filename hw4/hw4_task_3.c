cb2d528bfda4d51b7d6c47940e6908929564026d
Airforce Brett <bbishoplt1@yahoo.com>
2015-11-03 15:19:45 -0600

    Update hw4_task_3.c

--- hw4/hw4_task_3.c
+++ hw4/hw4_task_3.c
@@ -1,7 +1,5 @@
 
-//Brett Bishop
-//id 1000425627
-//cse1320
+
 //hw4 task 3 program to return bold underlined numbers and if they are a duplicate re-enter a number as well as print the max/min
 
 #include<stdio.h>
